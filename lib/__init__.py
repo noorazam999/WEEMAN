3a1e9d18fd4c716b9ebf64699ab1aab0feb9831c
ALIZA SHAH <45309121+noorazam999@users.noreply.github.com>
2018-11-26 00:08:49 -0800

    Add files via upload

---
+++ lib/__init__.py
@@ -0,0 +1,7 @@
+#
+# Copyright (c) 2016-2019 by aliza shah <ad433314@gmail.com>
+#
+# See 'LICENSE' for copying
+#
+
+
---
+++ modules/email.py
@@ -0,0 +1,25 @@
+#
+# email.py - weeman module - send email with HTML content.
+#
+# Copyright (c) 2019 aliza shah <ad433314@gmail.com>
+#
+# See 'LICENSE' for module copying
+#
+
+import smtplib
+
+# Module global configs
+MODULE_AUTHOR = "aliza shah <ad433314@gmail.com>"
+MODULE_LICENSE = "GPLv3"
+MODULE_VERSION = "4.4"
+MODULE_DATE = "22-01-2018"
+# Keep it short
+MODULE_DE   = "Send an email with HTML content."
+
+class Email(object):
+    """ Send HTML email """
+    def __init__(self):
+        pass
+
+def main():
+    pass
